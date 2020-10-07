67b1a7c8420a289c766b5c09a0510cd7277d6300
2284049 <70497897+2284049@users.noreply.github.com>
2020-10-06 18:50:48 -0700

    add Regex email pattern

--- getEmailError.js
+++ getEmailError.js
@@ -1,7 +1,11 @@
 function getEmailError(email) {
-   if (email.length === 0) {
-      return "Please enter your email address.";
-   } else {
-      return "";
-   }
+  const emailFormat = /^\w+.*@[a-zA-Z0-9][\w-]+\.[a-zA-Z]{2,}$/gm;
+  const isValidEmail = emailFormat.test(email);
+  if (email.length === 0) {
+    return "Please enter your email address.";
+  } else if (isValidEmail === false) {
+    return "Please enter a valid email address.";
+  } else {
+    return "";
+  }
 }
